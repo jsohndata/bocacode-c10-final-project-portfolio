98956cc402ee7c8bb01b5f55484fdce475fa3678
jSohnData <jihos@me.com>
2023-06-09 17:41:38 -0400

    WIP json data

---
+++ src/layout/Footer/Footer.js
@@ -0,0 +1,16 @@
+import {Container, Row, Col} from "../../utils/Bootstrap.jsx";
+
+export default function Footer() {
+  
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col>
+            <p>Footer</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
