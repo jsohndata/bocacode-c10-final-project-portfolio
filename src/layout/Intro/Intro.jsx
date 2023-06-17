35d64929d3e68a68501a78053b83d74ef72f10dd
jSohnData <jihos@me.com>
2023-06-16 22:02:43 -0400

    UPDATED intro

--- src/layout/Intro/Intro.jsx
+++ src/layout/Intro/Intro.jsx
@@ -16,7 +16,7 @@ export default function Intro() {
         <Row className="d-flex justify-content-center">
           <Col xs={11} md={10} lg={9} className="text-center">
             <h1>C-10 Portfolio</h1>
-              <p>Hats off to BocaCode's Cohort 10 for conquering a rollercoaster ride of a 10-week course and emerging victorious with their mind-blowing final project! We raise our glasses high and toast to the 13 exceptional students who have truly dazzled us with their brilliance. Bravo, you magnificent bunch!</p>        
+              <p>Congratulations to BocaCode's Cohort 10 for successfully completing a challenging 10-week course and impressing us with their incredible final project! We applaud the 13 outstanding students who have truly amazed us with their brilliance. Well done, you all!</p>        
           </Col>
         </Row>
       </Container>
