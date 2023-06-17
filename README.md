b00c51b0d995c47ecf44529827d6b0ae4e2316ba
jSohnData <jihos@me.com>
2023-06-16 22:46:54 -0400

    FIXED content

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # BocaCode C10 Final Project Portfolio
 
+Congratulations to BocaCode's Cohort 10 for successfully completing a challenging 10-week course and impressing us with their incredible final project! We applaud the 18 outstanding students who have truly amazed us with their brilliance. Well done, you all!
 
 ```
 https://via.placeholder.com/600x600
--- src/layout/Intro/Intro.jsx
+++ src/layout/Intro/Intro.jsx
@@ -16,7 +16,7 @@ export default function Intro() {
         <Row className="d-flex justify-content-center">
           <Col xs={11} md={10} lg={9} className="text-center">
             <h1>C-10 Portfolio</h1>
-              <p>Congratulations to BocaCode's Cohort 10 for successfully completing a challenging 10-week course and impressing us with their incredible final project! We applaud the 13 outstanding students who have truly amazed us with their brilliance. Well done, you all!</p>        
+              <p>Congratulations to BocaCode's Cohort 10 for successfully completing a challenging 10-week course and impressing us with their incredible final project! We applaud the 18 outstanding students who have truly amazed us with their brilliance. Well done, you all!</p>        
           </Col>
         </Row>
       </Container>
