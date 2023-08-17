d8273604e33c97a699f6367856530330ec27aeca
jSohnData <jihos@me.com>
2023-08-17 00:50:39 -0400

    ADDED mozart's ghost

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -12,6 +12,10 @@ export default function Footer() {
             <a href="https://github.com/jsohndata/bocacode-c10-final-project-portfolio" target="_blank" rel="noreferrer" className="text-white"><Github color="white" size={40} />
             <br />
             See Code in GitHub</a></p>
+            <p id="mozarts-ghost"><a href="https://notiempo.lol/mozartsghost/18"
+              rel="noreferrer"
+              alt="Click on it and press control-shift."
+              title="Click on it and press control-shift.">π</a></p>
           </Col>
         </Row>
       </Container>
