5de878e8296b59a447bccd3eb40236b993888e5a
Jiho Sohn <jsohndata@gmail.com>
2024-07-04 18:38:14 -0400

    UPDATE morzart ghost id

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -12,7 +12,7 @@ export default function Footer() {
             <a href="https://github.com/jsohndata/bocacode-c10-final-project-portfolio" target="_blank" rel="noreferrer" className="text-white"><Github color="white" size={40} />
             <br />
             See Code in GitHub</a></p>
-            <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=18"
+            <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=bcd10"
               rel="noreferrer"
               alt="Click on it and press control-shift."
               title="Click on it and press control-shift.">π</a></p>
